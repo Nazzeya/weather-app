f8af8a0ed51e521c69f2d667c2c4d250cd6ef56c
nazzeya <n.baisakalova01@gmail.com>
2022-12-26 03:54:13 +0600

    some styles was added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit} from '@angular/core';
 import { HttpDataService} from './services/httpData.service';
 import { Main } from './models/main';
 import { WeatherMain } from './models/weather_main';
+import { Weather } from './models/weather';
    
 @Component({
     selector: 'app-root',
@@ -12,7 +13,6 @@ import { WeatherMain } from './models/weather_main';
 export class AppComponent implements OnInit { 
 
     // for clearing the input entering place
-    public  color = '#aaa'
     inputText!: string;
     clear () {
     this.inputText = '';
@@ -20,6 +20,7 @@ export class AppComponent implements OnInit {
 
     weatherMain: WeatherMain | undefined;
     main: Main | undefined;
+    weathers: Weather[] = [];
       
     constructor(private httpService: HttpDataService){}
 
@@ -31,6 +32,7 @@ export class AppComponent implements OnInit {
         this.url = 'https://api.openweathermap.org/data/2.5/weather?q=' + cityName + '&appid=' + this.apiKey + '&units=metric'
         this.httpService.getData(this.url).subscribe({next:(data:any) => this.weatherMain=new WeatherMain(data.coord, data.weather, data.base, data.main, data.visibility, data.Wind, data.clouds, data.dt, data.sys, data.timezone, data.id, data.name, data.cod)});
         this.httpService.getMain(this.url).subscribe({next:(data:any) => this.main=new Main(data.temp, data.feels_like, data.temp_min, data.temp_max, data.pressure, data.humidity)})
+        this.httpService.getWeather(this.url).subscribe({next:(data: Weather[]) => this.weathers=data})
     }
 
     ngOnInit(){
